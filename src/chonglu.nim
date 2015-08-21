9825f025f87c8f1a6b1aab216fd98923c46ad784
Bruce Doan <rgv151@gmail.com>
2015-08-21 16:31:58 +0700

    test packet parsing

--- src/chonglu.nim
+++ src/chonglu.nim
@@ -2,7 +2,8 @@ import posix
 import strutils
 import parseopt2
 import redis
-import pfring/core
+
+import ../../pfring.nim/pfring/core
 
 import config
 
@@ -44,34 +45,55 @@ proc parseCommandLine(configFile: var string) =
     of cmdArgument: discard
     of cmdEnd: break
 
-proc callback(h: ptr pfring_pkthdr, p: ptr cstring, user_bytes: ptr cstring) =
-  p.parsePacket(h, 4, 1, 1)
-  var flags = h.extended_hdr.parsed_pkt.tcp.flags
+proc signalHandler() {.noconv.} =
+  echo "Blah"
+#  var stat = ring.getStats()
+#  echo "Received " & $stat.received & " packets, dropped " & $stat.dropped & " packets"
+#  ring.close()
+  quit(QuitSuccess)
+
+
+proc packetListener(h: ptr pfring_pkthdr, p: ptr cstring, user_bytes: ptr cstring) =
+  p.parsePacket(h, 4, 0, 0)
+  var packet = addr h.extended_hdr.parsed_pkt
+  var flags = packet.tcp.flags
   var syn =  (flags and TH_SYN) != 0
   var ack =  (flags and TH_ACK) != 0
+  var fin = (flags and TH_FIN) != 0
 
-  if syn and not ack:
-    echo "SYN"
-  elif syn and ack:
-    echo "SYN-ACK"
-  elif ack and not syn:
-    echo "ACK"
-  else:
-    echo "UKN"
+  var s = "..."
+
+  if syn:
+    s[0] = 'S'
+  if ack:
+    s[1] = 'A'
+  if fin:
+    s[2] = 'F'
+  echo s
+  if packet.ip_version == 4:
+    var src_addr, dst_addr: InAddr
+    src_addr.s_addr = htonl(packet.ip_src.v4.int32)
+    dst_addr.s_addr = htonl(packet.ip_dst.v4.int32)
+    #echo packet.dmac, " ", packet.smac
+    echo packet.ip_src.v4, " ", packet.ip_dst.v4
+    echo inet_ntoa(src_addr), " ", inet_ntoa(dst_addr)
+    echo packet.l4_src_port, " ", packet.l4_dst_port
+    echo packet.l3_proto, " ", packet.ip_tos
+
 
 proc main(config: Config) =
   var ring = newRing(config.interfaces, 65536, PF_RING_PROMISC or PF_RING_DO_NOT_PARSE)
+
   if ring.cptr.isNil:
     quit("pfring_open error: $#" % $errno, QuitFailure)
 
-  ring.setDirection(ReceiveOnly)
-  ring.setSocketMode(ReadOnly)
-  ring.setBPFFilter(config.filter)
+  setControlCHook(signalHandler)
+
+  #ring.setDirection(ReceiveOnly)
+  #ring.setSocketMode(ReadOnly)
+  #ring.setBPFFilter(config.filter)
   ring.enable()
-
-  ring.setLooper(callback, nil, true);
-
-  ring.close()
+  ring.startLoop(packetListener, nil, true);
 
 when isMainModule:
   var cfgFile: string
